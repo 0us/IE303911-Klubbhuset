bdbad29ba59ba1a264690b084235bd0b95a2f0cb
Steffen Holanger <holanger.steffen@gmail.com>
2019-10-23 16:42:26 +0200

    Trying to rename packages

--- app/src/main/java/com/example/ie303911_medlemsapp/ui/userviews/profile/ProfileViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.ie303911_medlemsapp.ui.userviews.profile;
-
-import androidx.lifecycle.ViewModel;
-
-public class ProfileViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
