99919b818d3117822bd8b1ceaac5f705323f7699
mknauser <33400600+mknauser@users.noreply.github.com>
2019-10-23 16:44:31 +0200

    rename

---
+++ server/README.md
@@ -0,0 +1,11 @@
+DEPENDENCIES
+
+- OpenJDK 8
+- Payara 5.193
+- MySQL 5.7.28
+- mysql-connector-java-5.1.48
+
+NOTE:
+MySQL-Connector must be placed in 
+- payara-5.193.1\payara5\glassfish\domains\domain1\lib
+- payara-5.193.1\payara5\glassfish\lib
---
+++ server/src/main/java/no/ntnu/randa/domain/Member.java
@@ -0,0 +1,44 @@
+package no.ntnu.randa.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Member {
+
+    @EmbeddedId
+    MemberKey id;
+
+    @ManyToOne
+    @MapsId("uid")
+    @JoinColumn(name = "uid")
+    private User user;
+
+    @ManyToOne
+    @MapsId("oid")
+    @JoinColumn(name = "oid")
+    private Organization organization;
+
+    @ManyToOne
+    @MapsId("gid")
+    @JoinColumn(name = "gid")
+    private Group group;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date created;
+
+    @PrePersist
+    protected void onCreate() {
+        created = new Date();
+    }
+
+    private boolean hasPaid;
+    private boolean needsToPay;
+}
