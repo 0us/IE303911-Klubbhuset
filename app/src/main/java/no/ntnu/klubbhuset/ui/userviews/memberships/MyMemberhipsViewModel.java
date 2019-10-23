ceaf9e5ab6a2336fafdc4ad7a9ac10faa0e35865
0us <larsoyvind28@gmail.com>
2019-10-23 17:41:07 +0200

    fix manifest error

--- app/src/main/java/no/ntnu/klubbhuset/ui/userviews/memberships/MyMemberhipsViewModel.java
+++ app/src/main/java/no/ntnu/klubbhuset/ui/userviews/memberships/MyMemberhipsViewModel.java
@@ -1,25 +1,14 @@
 package no.ntnu.klubbhuset.ui.userviews.memberships;
 
 import android.app.Application;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.media.Image;
-import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.ie303911_medlemsapp.R;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
