c439a07eb4ffbdc40e385a7a151e1a560b674b4c
0us <35314375+0us@users.noreply.github.com>
2019-10-26 20:33:16 +0200

    implement ProfileViewModel

--- app/src/main/java/no/ntnu/klubbhuset/ui/userviews/profile/ProfileViewModel.java
+++ app/src/main/java/no/ntnu/klubbhuset/ui/userviews/profile/ProfileViewModel.java
@@ -1,7 +1,48 @@
 package no.ntnu.klubbhuset.ui.userviews.profile;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ProfileViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import no.ntnu.klubbhuset.data.CommunicationConfig;
+import no.ntnu.klubbhuset.data.model.User;
+
+public class ProfileViewModel extends AndroidViewModel {
+    private RequestQueue requestQueue;
+
+    private MutableLiveData<User> user;
+
+    public ProfileViewModel(Application context) {
+        super(context);
+        this.requestQueue = Volley.newRequestQueue(context);
+    }
+
+    public MutableLiveData<User> getUser() {
+        if (this.user == null) {
+            this.user = new MutableLiveData<>();
+            fetchUser();
+        }
+        return user;
+    }
+
+    private void fetchUser() {
+        String url = CommunicationConfig.API_URL + "currentuser";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+            User newUser = new User(response);
+            user.setValue(newUser);
+        },
+                error -> {
+                    // error
+                    System.out.println(error.networkResponse);
+                });
+        requestQueue.add(request);
+    }
 }
