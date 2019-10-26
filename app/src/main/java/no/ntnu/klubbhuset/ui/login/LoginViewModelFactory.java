940320e733d5ef86baa858bd0e7a9ad1f9dda272
0us <35314375+0us@users.noreply.github.com>
2019-10-26 22:40:58 +0200

    remember if user is logged in, and prompt user with login if not

--- app/src/main/java/no/ntnu/klubbhuset/ui/login/LoginViewModelFactory.java
+++ app/src/main/java/no/ntnu/klubbhuset/ui/login/LoginViewModelFactory.java
@@ -1,5 +1,7 @@
 package no.ntnu.klubbhuset.ui.login;
 
+import android.app.Application;
+
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.annotation.NonNull;
@@ -13,12 +15,18 @@ import no.ntnu.klubbhuset.data.LoginRepository;
  */
 public class LoginViewModelFactory implements ViewModelProvider.Factory {
 
+    private Application context;
+
+    public LoginViewModelFactory(Application context) {
+        this.context = context;
+    }
+
     @NonNull
     @Override
     @SuppressWarnings("unchecked")
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         if (modelClass.isAssignableFrom(LoginViewModel.class)) {
-            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+            return (T) new LoginViewModel(context, LoginRepository.getInstance(new LoginDataSource()));
         } else {
             throw new IllegalArgumentException("Unknown ViewModel class");
         }
