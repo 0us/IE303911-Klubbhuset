dea78b66a5e6a712ee7624b9440c6ec672bca31d
mknauser <33400600+mknauser@users.noreply.github.com>
2019-11-02 13:43:03 +0100

    Club now has proper constructor.

--- app/src/main/java/no/ntnu/klubbhuset/data/model/Club.java
+++ app/src/main/java/no/ntnu/klubbhuset/data/model/Club.java
@@ -1,5 +1,6 @@
 package no.ntnu.klubbhuset.data.model;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import lombok.AllArgsConstructor;
@@ -8,10 +9,23 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Club {
+    private String description;
+    private long priceOfMembership;
+    private String emailContact;
+    private String url;
     private long oid;
     private String name;
 
     public Club(JSONObject json) {
-        // TODO: implement
+        try {
+            this.oid = json.getLong("oid");
+            this.name = json.getString("name");
+            this.url = json.getString("url");
+            this.emailContact = json.getString("emailContact");
+            this.priceOfMembership = json.getLong("priceOfMembership");
+            this.description = json.getString("description");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
