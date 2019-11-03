9305f4934c976d121db2a119f97d954777f38c37
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-03 20:44:19 +0100

    Setting up for using fragments in login activity

---
+++ app/src/main/java/no/ntnu/klubbhuset/adapter/AuthenticationPageAdapter.java
@@ -0,0 +1,31 @@
+package no.ntnu.klubbhuset.adapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthenticationPageAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+
+    public AuthenticationPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
+    }
+}
