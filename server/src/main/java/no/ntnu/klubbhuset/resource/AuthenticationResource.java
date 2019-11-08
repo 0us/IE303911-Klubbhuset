3928131ec099597bfc12bf44026ae11569e4ba28
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-08 13:10:34 +0100

    added test function. This should be removed after config done

--- server/src/main/java/no/ntnu/klubbhuset/resource/AuthenticationResource.java
+++ server/src/main/java/no/ntnu/klubbhuset/resource/AuthenticationResource.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -21,6 +22,12 @@ public class AuthenticationResource {
 
     @Inject
     AuthenticationService authenticationService;
+
+    @GET
+    public Response testConnection() {
+        System.out.println("ApplicationConfiguration.testConnection");
+        return Response.ok("all is good").build();
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
