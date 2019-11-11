1fb1b10be217e7bfa299fb386185928519f5531f
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-11 15:25:17 +0100

    Changed some configs
    Untested

--- server/src/main/java/no/ntnu/klubbhuset/DatasourceProducer.java
+++ server/src/main/java/no/ntnu/klubbhuset/DatasourceProducer.java
@@ -13,9 +13,11 @@ import static no.ntnu.klubbhuset.DatasourceProducer.JNDI_NAME;
 @DataSourceDefinition(
         name = JNDI_NAME,
         className = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource",
+        serverName = "klubbhusetdb",
+        databaseName = "klubbhuset",
         user = "user",
-        password = "password",
-        url = "jdbc:mysql://klubbhusetdb:3306/klubbhuset?useSSL=false"
+        password = "password"
+//        url = "jdbc:mysql://klubbhusetdb:3306/klubbhuset?useSSL=false"
 )
 public class DatasourceProducer {
     public static final String JNDI_NAME = "java:app/jdbc/klubbhuset";
