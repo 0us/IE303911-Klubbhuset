9ce5a1d0305faf55ad0aad9e5a627860e134c9d4
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-13 18:01:23 +0100

    get all members working

--- server/src/main/java/no/ntnu/klubbhuset/service/AdminOrganizationService.java
+++ server/src/main/java/no/ntnu/klubbhuset/service/AdminOrganizationService.java
@@ -1,14 +1,12 @@
 package no.ntnu.klubbhuset.service;
 
-import no.ntnu.klubbhuset.domain.Group;
 import no.ntnu.klubbhuset.domain.Member;
+import no.ntnu.klubbhuset.domain.Organization;
 
-import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.core.Response;
-import java.util.List;
 import java.util.Set;
 
 @Stateless
@@ -18,8 +16,14 @@ public class AdminOrganizationService {
     @PersistenceContext
     EntityManager entityManager;
 
+    /**
+     * Getting all members of an organization
+     * @param organizationId
+     * @return
+     */
     public Response getAllMembers(Long organizationId) {
-        List members = entityManager.createQuery("select m from Member m where oid = :oid").setParameter("oid", organizationId).getResultList();
+        Organization organization = entityManager.find(Organization.class, organizationId);
+        Set<Member> members = organization.getMembers();
 
         if (members.isEmpty()) {
             return Response.noContent().entity("There are no members in the organization").build();
