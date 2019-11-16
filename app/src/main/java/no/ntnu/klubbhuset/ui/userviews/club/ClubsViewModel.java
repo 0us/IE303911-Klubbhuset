429781b37e2d9971ba0aa82037538b64783b6543
0us <35314375+0us@users.noreply.github.com>
2019-11-16 16:34:54 +0100

    api calls now include authorization header class "AuthHelper" includes code to help create auth header in volley requests MasterActivity now clears token in sharedPreferences on logout

--- app/src/main/java/no/ntnu/klubbhuset/ui/userviews/club/ClubsViewModel.java
+++ app/src/main/java/no/ntnu/klubbhuset/ui/userviews/club/ClubsViewModel.java
@@ -1,22 +1,28 @@
 package no.ntnu.klubbhuset.ui.userviews.club;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
 import no.ntnu.klubbhuset.data.model.Club;
+import no.ntnu.klubbhuset.util.AuthHelper;
 
 import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static no.ntnu.klubbhuset.data.CommunicationConfig.API_URL;
 import static no.ntnu.klubbhuset.data.CommunicationConfig.ORGANIZATION;
@@ -49,6 +55,7 @@ public class ClubsViewModel extends AndroidViewModel {
 
     private void loadClubs() {
         String url = API_URL + ORGANIZATION;
+
         JsonArrayRequest jar = new JsonArrayRequest(Request.Method.GET, url, null,
                 response -> {
                     List<Club> clubs = new ArrayList<>();
@@ -60,7 +67,12 @@ public class ClubsViewModel extends AndroidViewModel {
                         System.out.println(jex);
                     }
                     this.clubs.setValue(clubs);
-                }, System.out::println);
+                }, System.out::println) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return AuthHelper.getAuthHeaders(getApplication());
+            }
+        };
         requestQueue.add(jar);
     }
 }
--- app/src/main/java/no/ntnu/klubbhuset/ui/userviews/profile/ProfileViewModel.java
+++ app/src/main/java/no/ntnu/klubbhuset/ui/userviews/profile/ProfileViewModel.java
@@ -1,20 +1,27 @@
 package no.ntnu.klubbhuset.ui.userviews.profile;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Base64;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.io.InvalidObjectException;
+import java.util.HashMap;
+import java.util.Map;
 
 import no.ntnu.klubbhuset.data.CommunicationConfig;
 import no.ntnu.klubbhuset.data.model.User;
+import no.ntnu.klubbhuset.util.AuthHelper;
 
 public class ProfileViewModel extends AndroidViewModel {
     private RequestQueue requestQueue;
@@ -36,6 +43,7 @@ public class ProfileViewModel extends AndroidViewModel {
 
     private void fetchUser() {
         String url = CommunicationConfig.API_URL + "currentuser";
+
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 response -> {
                     User newUser = null;
@@ -45,11 +53,16 @@ public class ProfileViewModel extends AndroidViewModel {
                         e.printStackTrace();
                     }
                     user.setValue(newUser);
-        },
+                },
                 error -> {
                     // error
                     System.out.println(error.networkResponse);
-                });
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return AuthHelper.getAuthHeaders(getApplication());
+            }
+        };
         requestQueue.add(request);
     }
 }
