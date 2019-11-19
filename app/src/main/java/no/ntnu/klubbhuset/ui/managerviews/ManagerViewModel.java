8f6635f39bcfe029eaffc12d2544a1a20e3af8f2
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-19 15:26:47 +0100

    Adding creatOrganizationFormDataState

--- app/src/main/java/no/ntnu/klubbhuset/ui/managerviews/ManagerViewModel.java
+++ app/src/main/java/no/ntnu/klubbhuset/ui/managerviews/ManagerViewModel.java
@@ -3,17 +3,16 @@ package no.ntnu.klubbhuset.ui.managerviews;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Patterns;
 import android.widget.Toast;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import com.android.volley.NetworkResponse;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -21,12 +20,11 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import no.ntnu.klubbhuset.data.CommunicationConfig;
+import no.ntnu.klubbhuset.R;
 import no.ntnu.klubbhuset.data.model.Club;
 import no.ntnu.klubbhuset.util.AuthHelper;
 
@@ -37,6 +35,7 @@ import static no.ntnu.klubbhuset.data.CommunicationConfig.ORGANIZATION;
 public class ManagerViewModel extends AndroidViewModel {
 
     private final SharedPreferences pref;
+    private MutableLiveData<CreateOrganizationFormState> createOrganizationFormState = new MutableLiveData<>();
     private MutableLiveData<List<Club>> clubs;
     private MutableLiveData<Club> createdClub;
     private RequestQueue requestQueue;
@@ -105,5 +104,34 @@ public class ManagerViewModel extends AndroidViewModel {
             }
         };
         requestQueue.add(jar);
+    }
+
+    public void organizationDataChanged(String email) {
+        boolean emailValid = isEmailValid(email);
+        if (!emailValid) {
+            createOrganizationFormState.setValue(new CreateOrganizationFormState(R.string.invalid_email));
+        } else {
+            createOrganizationFormState.setValue(new CreateOrganizationFormState(true));
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        if (email == null) {
+            return false;
+        }
+        boolean contains = email.contains("@");
+        if(!contains) {
+            return false;
+        }
+
+        if(email.contains("@")) {
+            return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        } else {
+            return !email.trim().isEmpty();
+        }
+    }
+
+    public MutableLiveData<CreateOrganizationFormState> getCreateOrganizationFormState() {
+        return createOrganizationFormState;
     }
 }
