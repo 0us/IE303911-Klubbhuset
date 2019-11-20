a8080ae3ba6fb8e94cf59d5cf140dd1fdec9d5bd
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-20 16:53:21 +0100

    capture payment

--- app/src/main/java/no/ntnu/klubbhuset/service/VippsService.java
+++ app/src/main/java/no/ntnu/klubbhuset/service/VippsService.java
@@ -22,6 +22,9 @@ public class VippsService {
     public static final String OCP_APIM_SUBSCRIPTION_KEY = ""; //todo
     public static final String VIPPS_API_URL = "https://apitest.vipps.no";
     public static final String AUTHORIZATION = "Authorization";
+    public static final String OCP_APIM_SUBSCRIPTION_KEY_STRING = "Ocp-Apim-Subscription-Key";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String CONTENT_TYPE = "Content-Type";
 
     private RequestQueue queue;
     private Context context; // calling class needs to give context;
@@ -58,7 +61,7 @@ public class VippsService {
         queue.add(request);
     }
 
-    public void initiatePayment() {
+    public void initiatePayment(int amount) {
         final String METHOD_URL = VIPPS_API_URL + "/ecomm/v2/payments/";
         JSONObject details = new JSONObject();
 
@@ -87,7 +90,7 @@ public class VippsService {
                 e.printStackTrace();
             }
 
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,METHOD_URL, details,
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, METHOD_URL, details,
                     response -> {
                         try {
                             String vippsURL = response.getString("url"); // todo don't know what we do with this. Can try to open it via intent or pase it to calling class
@@ -100,7 +103,7 @@ public class VippsService {
                 @Override
                 public Map<String, String> getHeaders() {
                     Map<String, String> headers = new HashMap<>();
-                    headers.put("Content-Type", "application/json");
+                    headers.put(CONTENT_TYPE, APPLICATION_JSON);
                     headers.put("Ocp-Apim-Subscription-Key", OCP_APIM_SUBSCRIPTION_KEY);
                     headers.put(AUTHORIZATION, "Bearer " + authToken);
                     return headers;
@@ -108,6 +111,47 @@ public class VippsService {
             };
             queue.add(request);
         }
+    }
+
+    public void capturePayment(String orderId, int amount) {
+        JSONObject body = new JSONObject();
+
+        try {
+            JSONObject merchantInfo = new JSONObject();
+            merchantInfo.put("merchantSerialNumber", merchantSerialNumber);
+
+            JSONObject transaction = new JSONObject();
+            transaction.put("amount", amount);
+            transaction.put("transactionText", transactionText);
+            transaction.put("skipLandingPage", true);
+
+            body.put("merchantInfo", merchantInfo);
+            body.put("transaction", transaction);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        final String METHOD_URL = VIPPS_API_URL + "/ecomm/v2/payments/" + orderId + "/capture";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, METHOD_URL, body,
+                response -> {
+                    Log.d(TAG, "capturePayment: response: " + response);
+                },
+                error -> {
+                }) // todo implement error handling
+        {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put(CONTENT_TYPE, APPLICATION_JSON);
+                headers.put(AUTHORIZATION, authToken);
+                headers.put(OCP_APIM_SUBSCRIPTION_KEY_STRING, OCP_APIM_SUBSCRIPTION_KEY);
+                return headers;
+            }
+        };
+
+        queue.add(request);
     }
 
     public void getPaymentStatus(String orderId) {
