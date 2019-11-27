e7a24b1c850e45bdfac3e5a9f1cdb3221b7f4040
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-27 17:20:01 +0100

    Change view back to login on response

--- app/src/main/java/no/ntnu/klubbhuset/service/UserService.java
+++ app/src/main/java/no/ntnu/klubbhuset/service/UserService.java
@@ -1,15 +1,26 @@
 package no.ntnu.klubbhuset.service;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.Response;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import no.ntnu.klubbhuset.ui.login.LoginActivity;
 
 import static no.ntnu.klubbhuset.data.CommunicationConfig.API_URL;
 import static no.ntnu.klubbhuset.data.CommunicationConfig.USER;
@@ -19,8 +30,10 @@ public class UserService {
     RequestQueue queue; // fixme needs context
 
     final String URL =  API_URL + USER;// todo needs other domain
+    Context context;
 
     public UserService(Context context) {
+        this.context = context;
         queue = Volley.newRequestQueue(context);
     }
 
@@ -36,20 +49,48 @@ public class UserService {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        String requestBody = newUserJson.toString();
 
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL, newUserJson,
+        StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, URL,
                 response -> {
                     Log.d(TAG, "createNewUser: got response" + response);
-                    // todo. Start main activity. send response to the activity
+                    Toast.makeText(context,"Profile created. You can now login", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);
                 },
                 error -> {
                     Log.d(TAG, "createNewUser: error");
                     if ( error != null) {
                         // todo handle error
-                        Log.d(TAG, "createNewUser: Error" + error);
+                        Log.d(TAG, "createNewUser: Error: " + error);
                     }
-                });
+                }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = "";
+                if (response != null) {
+                    responseString = String.valueOf(response.statusCode);
+                    // can get more details such as response.headers
+                }
+                return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+            }
+        };
 
         queue.add(jsonObjectRequest);
         return newUserJson;
