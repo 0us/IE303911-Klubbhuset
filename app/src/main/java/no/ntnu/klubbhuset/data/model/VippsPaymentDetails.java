6500b5449d5bd6176645678e02bfbfa2daef9332
Steffen Holanger <holanger.steffen@gmail.com>
2019-11-28 14:31:53 +0100

    get vipps access token and update order details

--- app/src/main/java/no/ntnu/klubbhuset/data/model/VippsPaymentDetails.java
+++ app/src/main/java/no/ntnu/klubbhuset/data/model/VippsPaymentDetails.java
@@ -1,5 +1,7 @@
 package no.ntnu.klubbhuset.data.model;
 
+import android.content.Context;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -7,6 +9,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import no.ntnu.klubbhuset.data.CommunicationConfig;
+import no.ntnu.klubbhuset.util.PreferenceUtils;
 
 public class VippsPaymentDetails {
     String mobileNummer;
@@ -20,25 +23,36 @@ public class VippsPaymentDetails {
 
 
 
-    public VippsPaymentDetails(String mobileNummer, OrderId orderId, double amount, String transactionText) {
+    public VippsPaymentDetails(String mobileNummer, OrderId orderId, double amount, String transactionText, Context context) {
         this.mobileNummer = mobileNummer;
         this.orderId = orderId;
         this.amount = amount;
         this.transactionText = transactionText;
+        merchantSerialNumber = CommunicationConfig.getInstance().getMerchantSerialNumber();
+        vippsAuthToken = PreferenceUtils.getVippsAccessToken(context);
+
 
         JsonObject customerInfo = new JsonObject();
         customerInfo.addProperty("mobileNumber", mobileNummer);
 
-        JsonObject merchantInfo = new JsonObject();
+        JsonObject staticShippingDetails = new JsonObject();
+        staticShippingDetails.addProperty("isDefault", "Y");
+        staticShippingDetails.addProperty("shippingCost", 0);
+        staticShippingDetails.addProperty("shippingMethod", "digital");
+        staticShippingDetails.addProperty("shippingMethodId", "DIG");
 
+        JsonObject merchantInfo = new JsonObject();
         merchantInfo.addProperty("callbackPrefix", CommunicationConfig.API_URL); //fixme
-        merchantInfo.addProperty("fallbackPrefix", "no.ntnu.klubbhuset."); //fixme
+        merchantInfo.addProperty("fallBack", "no.ntnu.klubbhuset.ui.userviews.club.detailed.ClubDetailedActivity"); //fixme
         merchantInfo.addProperty("isApp", true);
         merchantInfo.addProperty("merchantSerialNumber", merchantSerialNumber);
+//        merchantInfo.add("staticShippingDetails", staticShippingDetails); // uncomment if shipping info is required
 
         JsonObject transaction = new JsonObject();
+        transaction.addProperty("orderId", orderId.toString());
         transaction.addProperty("amount", amount);
         transaction.addProperty("transactionText", transactionText);
+        transaction.addProperty("skipLandingPage", false);
 
         details = new JsonObject();
         details.add("customerInfo", customerInfo);
