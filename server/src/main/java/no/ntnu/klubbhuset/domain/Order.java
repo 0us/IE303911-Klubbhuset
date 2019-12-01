a4865214488de52c93883ccc7b80adc8d9a5c615
Steffen Holanger <holanger.steffen@gmail.com>
2019-12-01 19:33:34 +0100

    Table mapping for order class

--- server/src/main/java/no/ntnu/klubbhuset/domain/Order.java
+++ server/src/main/java/no/ntnu/klubbhuset/domain/Order.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
+@Table(name = "VIPPSORDERS")
 
 public class Order {
     @Id
