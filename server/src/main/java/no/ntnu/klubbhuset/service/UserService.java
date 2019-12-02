2e6608a0ea5966bcecfac8826852478c0bf776fc
Steffen Holanger <holanger.steffen@gmail.com>
2019-12-02 14:19:27 +0100

    fixes #147

--- server/src/main/java/no/ntnu/klubbhuset/service/UserService.java
+++ server/src/main/java/no/ntnu/klubbhuset/service/UserService.java
@@ -6,11 +6,7 @@ import no.ntnu.klubbhuset.SaveImages;
 import no.ntnu.klubbhuset.domain.Image;
 import no.ntnu.klubbhuset.domain.SecurityGroup;
 import no.ntnu.klubbhuset.domain.User;
-import no.ntnu.klubbhuset.resource.UserResource;
 import org.eclipse.microprofile.jwt.JsonWebToken;
-import org.glassfish.jersey.media.multipart.ContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataBodyPart;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
@@ -23,10 +19,7 @@ import javax.security.enterprise.identitystore.PasswordHash;
 import javax.sql.DataSource;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-import java.io.File;
-import java.io.InputStream;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Log
 @Stateless
@@ -52,7 +45,7 @@ public class UserService {
     public Response getCurrentUser(SecurityContext securityContext) {
         String email = principal.getClaim("sub");
 
-        if ( email == null ) {
+        if (email == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
@@ -77,8 +70,13 @@ public class UserService {
     public Response createNewUser(User user) {
         log.log(Level.INFO, "UserService.CreateUser: was called. Json edition");
         if (user == null) {
-            return Response.status(Response.Status.FORBIDDEN).build();
+            return Response.status(Response.Status.FORBIDDEN).entity("User can not be null").build();
         }
+
+        if (doesUserExist(user)) {
+            return Response.status(Response.Status.FORBIDDEN).entity("User with email " + user.getEmail() + " already exist!").build();
+        }
+
         SecurityGroup securityGroup = entityManager.find(SecurityGroup.class, SecurityGroup.USER);
         user.addSecurityGroup(securityGroup);
         String hashedpw = hashPassword(user.getPassword());
@@ -88,58 +86,20 @@ public class UserService {
         return Response.status(Response.Status.CREATED).build();
     }
 
-    private String hashPassword(String password) {
-        return hasher.generate(password.toCharArray());
+    private boolean doesUserExist(User user) {
+        User exist = entityManager.find(User.class, user.getEmail());
+
+        return exist != null;
     }
 
-    public Response createNewUser(String firstname, String lastname, String email, String password, String phonenumber, FormDataMultiPart multiPart) {
-        Logger.getLogger(UserResource.class.getName()).log(Level.SEVERE, "UserService.createNewUser: was called");
-
-
-        // Check if user with same email exist
-//        if (!entityManager.createQuery("select u from User u where u.email = :email").getResultList().isEmpty()) {
-//            return Response.status(Response.Status.NOT_ACCEPTABLE).entity("User with same email address allready exist").build();
-//        }
-
-        User user = new User();
-        user.setFirstName(firstname);
-        user.setLastName(lastname);
-        user.setEmail(email);
-        user.setPassword(hasher.generate(password.toCharArray()));
-        user.setPhonenumber(phonenumber);
-        SecurityGroup securityGroup = entityManager.find(SecurityGroup.class, SecurityGroup.USER);
-        user.addSecurityGroup(securityGroup);
-        entityManager.persist(user);
-
-        FormDataBodyPart bodyPart = multiPart.getField(PROFILE_PICTURE);
-        if ( bodyPart != null ) {
-            if ( !saveImages.checkBodyPartIsImage(bodyPart) ) {
-                entityManager.remove(user);  // the user is already persisted to the database. Since the file uploaded is not an image, removing the user is done
-                return Response.status(Response.Status.UNSUPPORTED_MEDIA_TYPE)
-                        .entity("File must be image, no image was uploaded")
-                        .build();
-            }
-
-            InputStream inputStream = bodyPart.getValueAs(InputStream.class);
-            System.out.println("UserService.createNewUser: saving image");
-
-            ContentDisposition fileDetails = bodyPart.getContentDisposition();
-            String filename = fileDetails.getFileName();
-            String target = user.getEmail() + File.separator + PROFILE_PICTURE;
-
-            Image avatar = saveImages.saveImage(inputStream, target, filename);
-            coupleImageAndOrganization(user, avatar);
-        }
-
-        return Response.status(Response.Status.CREATED)
-                .entity(user)
-                .build();
+    private String hashPassword(String password) {
+        return hasher.generate(password.toCharArray());
     }
 
     public Response deleteUser() {
         User user = entityManager.find(User.class, principal.getName());
 
-        if ( user == null ) {
+        if (user == null) {
             return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
         }
 
@@ -159,24 +119,24 @@ public class UserService {
 
     public Response updateUser(User newUser) {
         User oldUser = entityManager.find(User.class, principal.getName());
-        if(oldUser == null ) {
+        if (oldUser == null) {
             return Response.status(Response.Status.BAD_REQUEST).entity("No user found").build();
         }
 
-        if(newUser.getFirstName() != null) {
+        if (newUser.getFirstName() != null) {
             oldUser.setFirstName(newUser.getFirstName());
         }
 
-        if(newUser.getLastName() != null) {
+        if (newUser.getLastName() != null) {
             oldUser.setLastName(newUser.getLastName());
         }
 
-        if(newUser.getPassword() != null) {
+        if (newUser.getPassword() != null) {
             String hashedPassword = hashPassword(newUser.getPassword());
             oldUser.setPassword(hashedPassword);
         }
 
-        if(newUser.getPhonenumber() != null) {
+        if (newUser.getPhonenumber() != null) {
             oldUser.setPhonenumber(newUser.getPhonenumber());
         }
 
