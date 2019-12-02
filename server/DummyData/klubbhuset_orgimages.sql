27422b2c3a169906b18b1ea10c89bf905c443877
mknauser <33400600+mknauser@users.noreply.github.com>
2019-12-02 18:26:09 +0100

    Changed max length of description to much larger. Added dummydata in /server/dummydata/

---
+++ server/DummyData/klubbhuset_orgimages.sql
@@ -0,0 +1,4 @@
+INSERT INTO klubbhuset.orgimages (iid, oid) VALUES (10001, 10001);
+INSERT INTO klubbhuset.orgimages (iid, oid) VALUES (10002, 10002);
+INSERT INTO klubbhuset.orgimages (iid, oid) VALUES (10003, 10003);
+INSERT INTO klubbhuset.orgimages (iid, oid) VALUES (10004, 10004);
